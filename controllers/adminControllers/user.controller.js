c2a02b54caf928a73d03cc47a198a256a42e51f2
Rup medhi <rupmmedhi@gmail.com>
2025-06-29 13:07:35 +0530

    cookie update

--- controllers/adminControllers/user.controller.js
+++ controllers/adminControllers/user.controller.js
@@ -70,9 +70,9 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
     try {
         res.clearCookie("adminJwt", {
-            httpOnly: true,
-            sameSite: "Lax",
-            secure: false,
+            httpOnly: false,
+            secure: true,
+            sameSite: "None"
         });
         res.status(200).json({ success: true });
     } catch (error) {
